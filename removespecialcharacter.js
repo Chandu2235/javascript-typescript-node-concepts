a51d4c9e333b6b79510ab3c038e8aab293132fcf
Chandrakant Vijay BIrhade <chandu.birhade@gmail.com>
2025-12-04 19:29:09 +0530

    changes updated

---
+++ removespecialcharacter.js
@@ -0,0 +1,3 @@
+const input = "aut0omation@#!3t3est!ing";
+const output = input.replace(/[^a-zA-Z0-9]/g, "");
+console.log(output);
--- reverseSTring.js
+++ reverseSTring.js
@@ -6,3 +6,27 @@ for (let i = str.length - 1; i >= 0; i--) {
   reversed += str[i];
 }
 console.log(reversed); // "noitamotua"
+
+
+//reverse string in place
+let a = ["h","e","l","l","o"];
+reverseString(a);
+console.log(a); // ["o","l","l","e","h"]
+
+//reverse string in place
+let s = ["h","e","l","l","o"];
+reverseString(s);
+console.log(s); // ["o","l","l","e","h"]
+function reverseString(s) {
+  let left = 0;
+  let right = s.length - 1;       
+  while (left < right) {
+    // Swap characters
+    let temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+    left++;
+    right--;
+  }           
+  return s;
+}
